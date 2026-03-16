cbd3a1a96d4bc4e2acf8101ac47aa0e61aeb7d73
pauguz <paulo.guzman1@unmsm.edu.pe>
2026-03-15 22:12:48 -0500

    Correccion de la sidebar

--- src/components/editor/SideEditor/OptionSideBar.tsx
+++ src/components/editor/SideEditor/OptionSideBar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Sidebar, SidebarProvider } from '../../ui/sidebar.tsx'
 import SideItem from './SideItem.tsx'
-import {LucideIcon, Trophy, DollarSign, ChessPawn } from 'lucide-react'
+import {LucideIcon, Trophy, DollarSign, ChessPawn, UsersIcon } from 'lucide-react'
 import {menuType} from '../../../types/menu.ts'
 import { useGameEditor } from '@/context/GameEditorContext';
 
@@ -11,16 +11,15 @@ const OptionSideBar = () => {
   const sections: menuType[] = [
     { id: '', imageUrl: ChessPawn, name: 'Fichas'},
     { id: '', imageUrl: Trophy, name: 'Victoria'},
+    { id: '', imageUrl: UsersIcon, name: 'Jugadores'},
     { id: '', imageUrl: DollarSign, name: 'Donaciones'},
   ];
 
   return (
-    <div>
-      <div className="flex-1 overflow-y-auto p-2 space-y-1">
+    <div  className="flex flex-col">
+      <div className="flex flex-1 flex-col p-2 justify-around">
       {sections.map(sec => (
           <SideItem gen={sec} bloqueo={isPlaying} selectedID={SelectedMenuId} selection={setSelectedMenuId} /> 
-
-
         ))  }
       </div>
     </div>
