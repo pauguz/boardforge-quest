c476ce7be42e22df53d8a5a4f8b905c51f66cb9b
pauguz <114239281+pauguz@users.noreply.github.com>
2026-03-23 04:07:19 -0500

    Update GenRouter.tsx

--- src/routing/GenRouter.tsx
+++ src/routing/GenRouter.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Index from "../pages/Index";
 import NotFound from "../pages/NotFound";
 import Ayuda from "../pages/Ayuda";
-import LudiSala from '@/pages/LudiSala';
 import Editor from '@/pages/Editor';
 
 
