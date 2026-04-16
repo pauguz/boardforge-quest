74c42372400b4cf39b79c36c3cde80baa0620aee
pauguz <paulo.guzman1@unmsm.edu.pe>
2026-04-16 15:08:43 -0500

    Debugeo

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -4,5 +4,8 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || import.meta.env.SUPABAS
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || import.meta.env.SUPABASE_ANON_KEY;
 if (!supabaseUrl) {
     console.error("La URL de Supabase no está llegando al cliente.");
+    console.log("Variables detectadas por Vite:", import.meta.env);
   }
+
+  
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
