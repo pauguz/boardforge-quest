735e5589dc8216993404c7a1c9e56f8eff92f5e7
pauguz <paulo.guzman1@unmsm.edu.pe>
2026-04-16 15:00:12 -0500

    Update supabaseClient.ts
    Debugeo

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -2,5 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || import.meta.env.SUPABASE_ANON_KEY;
-
+if (!supabaseUrl) {
+    console.error("La URL de Supabase no está llegando al cliente.");
+  }
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
